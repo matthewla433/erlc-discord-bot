7c776ed04857e62c6af33eddd34ec90da6282d08
Jose Moran Urena <jose@josemoranurena.tech>
2024-07-03 20:05:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,23 +24,29 @@ for (const folder of commandFolders) {
 }
 
 client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+	if (interaction.isChatInputCommand() || interaction.isAutocomplete()) {
+		const command = interaction.client.commands.get(interaction.commandName);
+	
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
 
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		try {
+			if (interaction.isChatInputCommand()) {
+				await command.execute(interaction);
+			} else if (interaction.isAutocomplete()) {
+				await command.autocomplete(interaction);
+			}
+		} catch (error) {
+			console.error(error);
+			if (interaction.isChatInputCommand()) {
+				if (interaction.replied || interaction.deferred) {
+					await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+				} else {
+					await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				}
+			}
 		}
 	}
 });
